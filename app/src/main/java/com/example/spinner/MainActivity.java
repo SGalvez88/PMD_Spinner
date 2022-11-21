46c147dd031352e589b340309d66e8702d02586f
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-11-21 09:16:58 +0100

    commit inicial

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner miSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        miSpinner = (Spinner) findViewById(R.id.spnOpciones);
+        String [] valores = {"valor1","valor2","valor3"};
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,valores);
+        miSpinner.setAdapter(adaptador);
     }
 }
